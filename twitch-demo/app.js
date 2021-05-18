012502a4aa315a326101386b62c7ef56dafe875b
gelu89 <ic.gelu@gmail.com>
2021-05-18 17:05:01 +0300

    hfghfgh

--- twitch-demo/app.js
+++ twitch-demo/app.js
@@ -1,37 +1,41 @@
 
-
-const chat = document.querySelector("#chat");
+const twChat = document.querySelector("#chat");
 const messages = [];
 const maxMessages = 20;
 ComfyJS.onChat = (user, message, flags, self, extra) => {
     updateMessages(user, message);
-    // console.log(user, message)
+    if (message === "OMEGALUL") {
+        ComfyJS.Say("LUL");
+    }
+    // if (message.includes('a')) {
+    //     ComfyJS.Say("fdgd");
+    // }
 }
-ComfyJS.Init("xQcOW");
+ComfyJS.Init("Wolfabelle", "zaa5dqea0ypm54vp2uyvcnwpmnf6t4");
 
 function updateMessages(user, message) {
     messages.push(message);
-    // userN.innerText = username;
-    // const messageS = document.createElement('span');
-    // messageS.innerText = message;
-    const textT = document.createElement('span');
-    textT.classList.add('text');
-    textT.innerText = message;
+
     const userEl = document.createElement('b');
     userEl.classList.add('user');
     userEl.innerText = user + ': ';
-    const lineText = document.createElement('p');
+
+    const textT = document.createElement('span');
+    textT.classList.add('text');
+    textT.innerText = message;
+
+
+    const lineText = document.createElement('div');
     lineText.appendChild(userEl);
     lineText.appendChild(textT);
 
-    chat.appendChild(lineText);
-    // textT.appendChild(userN);
-    // textT.appendChild(messageS);
-    // text.innerText += message;
+    twChat.appendChild(lineText);
+
     if (messages.length > maxMessages) {
         messages.shift();
-        const messageToDelete = chat.firstChild;
-        chat.removeChild(messageToDelete);
+        const messageToDelete = twChat.firstChild;
+        twChat.removeChild(messageToDelete);
     }
+
 
 }
